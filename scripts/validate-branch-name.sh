5a8ddd8655cd7934b3adc84757eda5520d06478b
francisadediran <francis.adediran@publicissapient.com>
2020-04-16 15:32:06 +0100

    commitizen added branch validation

--- scripts/validate-branch-name.sh
+++ scripts/validate-branch-name.sh
@@ -8,15 +8,20 @@ NC='\033[0m' # No Color
 
 local_branch="$(git rev-parse --abbrev-ref HEAD)"
 
-valid_branch_regex="^master$"
-# valid_branch_regex="^(JIRA|BB|CHAN|EE)-[0-9._-]+$"
+# valid_branch_regex="^master$" (not be used)
+# JIRA - Confluence or documentation related
+# FEAT - Feature
+# CHAN - Change
+# [0-9._-] - id number
+# e.g. FEAT-123
+valid_branch_regex="^(JIRA|FEAT|CHAN)-[0-9._-]+$"
 
 message="\n\nThere is something wrong with your branch name.\nBranch names in this project must adhere to this contract:\n${bold}${YLW}$valid_branch_regex ${normal}${NC} \n\n${RED}Your commit will be rejected.${NC} \nYou should rename your branch to a valid name and try again.\n\n"
 
-# if [[ ! $local_branch =~ $valid_branch_regex ]]
-# then
-#     echo "$message"
-#     exit 1
-# fi
+if [[ ! $local_branch =~ $valid_branch_regex ]]
+then
+    echo "$message"
+    exit 1
+fi
 
 exit 0
